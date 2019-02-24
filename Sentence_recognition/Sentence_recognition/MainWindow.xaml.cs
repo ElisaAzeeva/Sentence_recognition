2708ef81a43d4958e3481a10ef8a582db24bb572
qwe345asd <vva.99@yandex.ru>
2019-02-24 17:41:01 +0300

    Interface test 1
    Тестовый вариант интерфейса.
    
    Смотрим как большие файлы взаимодействуют с текстовыми полями...
    
    Приходим к плану с листом строк, однако он тоже имеет свои недостатки.

--- Sentence_recognition/Sentence_recognition/MainWindow.xaml.cs
+++ Sentence_recognition/Sentence_recognition/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,15 +16,32 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Sentence_recognition
-{
+{   
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        string[] lines = File.ReadAllLines("War_and_Peace.txt");
+
         public MainWindow()
         {
             InitializeComponent();
+
+
+            text.ItemsSource = lines;
+        }
+
+        private void ScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            var s = (ScrollViewer)sender;
+
+            Debug.WriteLine($" Offset: {s.VerticalOffset}");
+
+            Debug.WriteLine($" Height: {s.ViewportHeight}");
+
+            Debug.WriteLine(lines[(int)s.VerticalOffset]);
+
         }
     }
 }
