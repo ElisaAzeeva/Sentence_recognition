ea831ec6be2c79850084d52c8800f8993325efa6
qwe345asd <vva.99@yandex.ru>
2019-02-25 00:12:28 +0300

    Warious underlines inmplimented.

--- Sentence_recognition/Sentence_recognition/MainWindow.xaml.cs
+++ Sentence_recognition/Sentence_recognition/MainWindow.xaml.cs
@@ -20,9 +20,84 @@ namespace Sentence_recognition
     /// </summary>
     public partial class MainWindow : Window
     {
+        enum SentenceMembers
+        {
+            Subject,
+            Predicate,
+            Definition,
+            Application,
+            Addition,
+            Circumstance,
+        }
+
+        class Token
+        {
+            public int Start { get; }
+            public int Length { get; }
+            public SentenceMembers Type { get; set; }
+
+            public Token(int start, int length, SentenceMembers type)
+            {
+                Start = start;
+                Length = length;
+                Type = type;
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+
+            string text = @"
+test test testаа test
+test test test test
+";
+                
+            var list = new List<Token>{
+                new Token(2,4,SentenceMembers.Subject),
+                new Token(7,4,SentenceMembers.Predicate),
+                new Token(12,6,SentenceMembers.Definition),
+                new Token(19,4,SentenceMembers.Addition),
+            };
+
+            block.Inlines.Clear();
+
+            TextDecorationCollection GetDecorationFromType(SentenceMembers type)
+            {
+                // TODO Add other 
+                switch (type)
+                {
+                    case SentenceMembers.Subject:
+                        return MyTextDecorations.DashDotedUnderline;
+                    case SentenceMembers.Predicate:
+                        return MyTextDecorations.Underline;
+                    case SentenceMembers.Definition:
+                        return MyTextDecorations.WavyUnderline;
+                    case SentenceMembers.Circumstance:
+                        //return MyTextDecorations.OverLine;
+                    case SentenceMembers.Application:
+                    case SentenceMembers.Addition:
+                        return MyTextDecorations.DashedUnderline;
+                    default:
+                        return null;
+                }
+            }
+
+            IEnumerable<Run> GetRuns()
+            {
+                int curent = 0;
+                foreach (var t in list)
+                {
+                    yield return new Run(text.Substring(curent, t.Start - curent));
+                    yield return new Run(text.Substring(t.Start, t.Length)) {
+                        TextDecorations = GetDecorationFromType(t.Type)
+                    };
+                    curent = t.Start + t.Length;
+                }
+                yield return new Run(text.Substring(curent));
+            }
+
+            block.Inlines.AddRange(GetRuns());
         }
     }
 }
---
+++ Sentence_recognition/Sentence_recognition/MyTextDecorations.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.Generic;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace System.Windows
+{
+    public static class MyTextDecorations
+    {
+
+        static MyTextDecorations()
+        {
+            {
+                // Init Underline
+                TextDecoration td = new TextDecoration();
+                td.Location = TextDecorationLocation.Underline;
+                Underline = new TextDecorationCollection();
+                Underline.Add(td);
+                Underline.Freeze();
+            }
+
+            {
+                // Init DoubleUnderline
+                DoubleUnderline = new TextDecorationCollection
+                {
+                    new TextDecoration
+                    {
+                        Location = TextDecorationLocation.Underline
+                    }
+                };
+                DoubleUnderline.Freeze();
+            }
+
+            // Init WavyUnderline
+            {
+                var h = 2;
+                var w = 2;
+
+                WavyUnderline = new TextDecorationCollection {
+                    new TextDecoration {
+                        Location = TextDecorationLocation.Underline,
+                        Pen = new Pen(new VisualBrush
+                        {
+                            Stretch = Stretch.None,
+                            TileMode = TileMode.Tile,
+                            ViewportUnits = BrushMappingMode.Absolute,
+                            Viewport = new Rect(0, 0, 3 * w, 3 * h),
+                            Visual = new Path
+                            {
+                                Stroke = Brushes.Black,
+                                StrokeThickness = 1/2d,
+                                StrokeEndLineCap = PenLineCap.Square,
+                                StrokeStartLineCap = PenLineCap.Square,
+                                Data = new PathGeometry(
+                                    new List<PathFigure>{
+                                        new PathFigure(
+                                            new Point(0,h),
+                                            new List<BezierSegment>{
+                                                new BezierSegment(
+                                                    new Point(w,0),
+                                                    new Point(2*w,2*h),
+                                                    new Point(3*w,h),
+                                                    true)
+                                            },
+                                            false
+                                            )
+                                    })
+                            }
+                        }, 6*h)
+                    }
+                };
+
+                //WavyUnderline.Freeze();
+            }
+
+            // Init DashedUnderline
+            {
+                DashedUnderline = new TextDecorationCollection {
+                    new TextDecoration
+                    {
+                        Location = TextDecorationLocation.Underline,
+                        Pen = new Pen(Brushes.Black, 1)
+                        {
+                            DashStyle = new DashStyle(new List<double> { 4, 4 }, 0)
+                        }
+                    }
+                };
+                DashedUnderline.Freeze();
+            }
+
+            // Init DashDotedUnderline
+            {
+                DashDotedUnderline = new TextDecorationCollection {
+                    new TextDecoration
+                    {
+                        Location = TextDecorationLocation.Underline,
+                        Pen = new Pen(Brushes.Black, 1)
+                        {
+                            DashStyle = new DashStyle(new List<double> { 4, 4, 0, 4 }, 0)
+                        }
+                    }
+                };
+                DashDotedUnderline.Freeze();
+            }
+        }
+
+
+        public static TextDecorationCollection Underline { get; }
+
+        public static TextDecorationCollection DoubleUnderline { get; }
+
+        public static TextDecorationCollection WavyUnderline { get; }
+
+        public static TextDecorationCollection DashedUnderline { get; }
+
+        public static TextDecorationCollection DashDotedUnderline { get; }
+    }
+}
