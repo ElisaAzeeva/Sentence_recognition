f1f3cbe8e825555290681b7711c3869f71fed521
Nova201810 <34684380+Nova201810@users.noreply.github.com>
2019-03-01 19:17:07 +0300

    Update MainWindow.xaml.cs

--- Sentence_recognition/Sentence_recognition/MainWindow.xaml.cs
+++ Sentence_recognition/Sentence_recognition/MainWindow.xaml.cs
@@ -20,9 +20,58 @@ namespace Sentence_recognition
     /// </summary>
     public partial class MainWindow : Window
     {
+        class word
+        {
+            string name { get; set; } //собственно само слово
+            string type { get; set; } //каким типом является слово
+            bool capital { get; set; } //начинается ли слово с заглавной буквы
+            bool change { get; set; } //может ли изменяться(по роду, числу...)
+            public word(string Name, string Type, bool Capital, bool Change)
+            {
+                name = Name;
+                type = Type;
+                capital = Capital;
+                change = Change;
+            }
+        };
+
+        List<List<word>> first_letter = new List<List<word>>(); //создание списка: (начальная буква слова)->(слово, тип, начинается ли с заглавной буквы, может ли изменяться)
         public MainWindow()
         {
             InitializeComponent();
+            //first_letter.AddRange(new List<List<word>> {
+            //    new List<word> {
+            //                     new word("А", "частица", true, false),
+            //                     new word("а", "союз", false, false),
+
+            //                   }
+            //});
+
+            //Добавление слов на букву А
+            List<word> letter_a = new List<word>();
+            letter_a.AddRange(new List<word> {
+                                 new word("А", "частица", true, false),
+                                 new word("а", "союз", false, false),
+                                 new word("а вдобавок", "союз", false, false),
+                                 new word("а именно", "союз", false, false),
+                                 new word("а также", "союз", false, false),
+                                 new word("а то", "союз", false, false),
+                               });
+            //Добавление слов на букву Б
+            List<word> letter_b = new List<word>();
+            letter_b.AddRange(new List<word> {
+                                 new word("Б", "", true, false),
+                                 new word("благодаря тому что", "союз", false, false),
+                                 new word("благо", "союз", false, false),
+                                 new word("буде", "союз", false, false),
+                                 new word("будто", "союз", false, false),
+                                 new word("без", "предлог", false, false),
+                                 new word("близ", "предлог", false, false),
+                                 new word("благодаря", "предлог", false, false),
+                                 new word("бы", "частица", false, false),
+                               });
+
+            first_letter.AddRange(new List<List<word>>{letter_a, letter_b});
         }
     }
 }
