28b72172fd70a4adf909eb9301539c6c7a4f50c3
KOTRU <lipatovsa@mail.ru>
2019-03-01 18:37:14 +0300

    Divide_class added
    Add cs file for Functions

---
+++ Sentence_recognition/Sentence_recognition/Divide_Class.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sentence_recognition
+{
+    class Divide_Class
+    {
+        public List<string> divide_sent(string words)
+        {
+
+            List<string> split1 = new List<string>();
+            string[] split = words.Split(new Char[] { ' ', ',', '.', ':', '\t', '(', ')' });
+
+            foreach (string s in split)
+            {
+
+                if (s.Trim() != "")
+                    split1.Add(s);
+
+            }
+            return split1;
+        }
+        public List<string> divide_text(string words)
+        {
+
+            List<string> split1 = new List<string>();
+            string[] split = words.Split(new Char[] { '!', '?', '.' });
+
+            foreach (string s in split)
+            {
+
+                if (s.Trim() != "")
+                    split1.Add(s);
+
+            }
+            return split1;
+        }
+    }
+}
