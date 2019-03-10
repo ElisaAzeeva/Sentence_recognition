847394ce919f9aa3a14a158bbaf2a7494c4d5d1c
MrElekzil <47747441+MrElekzil@users.noreply.github.com>
2019-03-10 21:09:12 +0300

    Update MainWindow.xaml.cs

--- Sentence_recognition/Sentence_recognition/MainWindow.xaml.cs
+++ Sentence_recognition/Sentence_recognition/MainWindow.xaml.cs
@@ -41,6 +41,10 @@ namespace Sentence_recognition
             }
 
         }
+        List<int> sv9Iz = new List<int>();
+        List<string> slova = new List<string>();
+        List<int> SOS = new List<int>();
+        IntPtr hEngine;
         private void Grid_Loaded(object sender, RoutedEventArgs e)
         {
             Divide_Class ll = new Divide_Class();
@@ -49,7 +53,7 @@ namespace Sentence_recognition
             List<string> split2 = new List<string>();
             split1 = ll.divide_text("Из молодежи, не считая старшей дочери графини (которая была четырьмя годами старше сестры и держала себя уже как большая) и гостьи-барышни, в гостиной остались Николай и Соня-племянница. Соня была тоненькая, миниатюрненькая брюнетка с мягким, отененным длинными ресницами взглядом, густою черною косою, два раза обвивавшею ее голову, и желтоватым оттенком кожи на лице и в особенности на обнаженных худощавых, но грациозных мускулистых руках и шее. Плавностью движений, мягкостью и гибкостью маленьких членов и несколько хитрою и сдержанною манерой она напоминала красивого, но еще не сформировавшегося котенка, который будет прелестною кошечкой. Она, видимо, считала приличным выказывать улыбкой участие к общему разговору; но против воли ее глаза из-под длинных густых ресниц смотрели на уезжающего в армию cousin с таким девическим страстным обожанием, что улыбка ее не могла ни на мгновение обмануть никого, и видно было, что кошечка присела только для того, чтоб еще энергичнее прыгнуть и заиграть с своим cousin, как скоро только они так же, как Борис с Наташей, выберутся из этой гостиной.");
             split2 = ll.divide_sent(split1[nomer], nomer);
-            IntPtr hEngine = GrammarEngine.sol_CreateGrammarEngineW(@"F:\RussianGrammaticalDictionary\bin-windows/dictionary.xml");
+             hEngine = GrammarEngine.sol_CreateGrammarEngineW(@"C:\bin-windows/dictionary.xml");
             if (hEngine == IntPtr.Zero)
             {
                 Console.WriteLine("Could not load the dictionary");
@@ -74,9 +78,8 @@ namespace Sentence_recognition
                 Console.WriteLine("Russian language is missing in lexicon.");
                 return;
             }
-            IntPtr hPack11 = GrammarEngine.sol_SyntaxAnalysis(hEngine, "Красная чайка села на дерево и улетела домой ", 0, 0, (60000 | (20 << 22)), GrammarEngineAPI.RUSSIAN_LANGUAGE);
-            string[] fffa;
-            int[] fffffa;
+            IntPtr hPack11 = GrammarEngine.sol_SyntaxAnalysis(hEngine, "Красивая чайка села на дерево и улетела ", GrammarEngine.MorphologyFlags.SOL_GREN_ALLOW_FUZZY, 0, (60000 | (20 << 22)), GrammarEngineAPI.RUSSIAN_LANGUAGE);
+            //IntPtr hPack1 = GrammarEngine.sol_MorphologyAnalysis(hEngine, "Красивая чайка села на дерево и улетела", GrammarEngine.MorphologyFlags.SOL_GREN_ALLOW_FUZZY, 0, (60000 | (20 << 22)), GrammarEngineAPI.RUSSIAN_LANGUAGE);
             int fffff = GrammarEngine.sol_CountGrafs(hPack11);
             int nroot = GrammarEngine.sol_CountRoots(hPack11, 0);
             //  IntPtr hRoot = GrammarEngine.sol_GetRoot(hPack11, 0, iroot);
@@ -89,43 +92,32 @@ namespace Sentence_recognition
                 IntPtr hRoot = GrammarEngine.sol_GetRoot(hPack11, 0, iroot);
                 string fff = GrammarEngine.sol_GetNodeContentsFX(hRoot);
                 int broot = GrammarEngine.sol_CountLeafs(hRoot);
-
-        
-                fffffa = new int[1000];
-                fffa = new string[1000];
-
-                for (int Leaf = 0; Leaf < broot; Leaf++)
-                {
-                     fffffa[Leaf] = GrammarEngine.sol_GetLeafLinkType(hRoot, Leaf);
-                    IntPtr rf = GrammarEngine.sol_GetLeaf(hRoot, Leaf);
-
-                    int broofft = GrammarEngine.sol_CountLeafs(rf);
-                    if (broofft > 1)
-                    {
-                        for (int Leaff = 0; Leaff < broofft; Leaff++)
-                        {
-                            IntPtr frf = GrammarEngine.sol_GetLeaf(rf, Leaff);
-                            fffa[Leaff] = GrammarEngine.sol_GetNodeContentsFX(frf);
-                        }
-                    }
-
-                        fffa[Leaf] = GrammarEngine.sol_GetNodeContentsFX(rf);
-                    for (int Leaff = 0; Leaff < broofft; Leaff++)
-                    {
-                        int fdf = GrammarEngine.sol_GetLeafLinkType(rf, 0);
-                        IntPtr frf = GrammarEngine.sol_GetLeaf(rf, Leaff);
-                        int brfffft = GrammarEngine.sol_CountLeafs(frf);
-                        fffa[Leaf+ broofft] = GrammarEngine.sol_GetNodeContentsFX(frf);
-                        IntPtr frfb = GrammarEngine.sol_GetLeaf(frf, 0);
-                        string fsass= GrammarEngine.sol_GetNodeContentsFX(frfb);
-                        int fdccf = GrammarEngine.sol_GetLeafLinkType(frf, 0);
-                    }
-                }
+             
+                        Razbor(hRoot);
+                    
+                
                 GrammarEngine.sol_DeleteResPack(hPack11);
 
             }
         }
-
+        private void Razbor(IntPtr rf)
+        {
+            int broofft = GrammarEngine.sol_CountLeafs(rf);// Количество листьев
+            for (int Leaff = 0; Leaff < broofft; Leaff++)
+            {
+                //НЕЕЕЕЕ ЛЕЕЕЕЗЬЬЬЬ 
+                IntPtr frf = GrammarEngine.sol_GetLeaf(rf, Leaff);//Указатель на текущий лист
+                int afafaf = GrammarEngine.sol_GetNodeIEntry(hEngine, frf);//Первичный ключ словарной статьи(для определения части речи)
+                SOS.Add(GrammarEngine.sol_GetEntryClass(hEngine, afafaf));// id части речи
+                slova.Add(GrammarEngine.sol_GetNodeContentsFX(frf)); //Слова
+                sv9Iz.Add(GrammarEngine.sol_GetLeafLinkType(rf, Leaff));//Связи листа и его корня (предыдущего листа)
+                int broofftt = GrammarEngine.sol_CountLeafs(frf);//Количество листьев у текущего листа
+                if (broofftt>0)
+                {
+                    Razbor(frf);//Рекурсия
+                }
+            }
+        }
     }
 }
 
